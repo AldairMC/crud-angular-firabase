45e9023334264b3512b36ea2d87d785e74d735ec
AldairMC <aldairmc2023@gmail.com>
2019-09-03 23:54:55 -0500

    Create finished

---
+++ src/app/components/tasks/task/task.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+//servicio
+import { TaskService } from '../../../services/task.service'
+
+//import ngForm para el reset
+import { NgForm } from '@angular/forms';
+
+// task class
+import  { Task } from '../../../models/task'
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.css']
+})
+export class TaskComponent implements OnInit {
+
+  constructor(private taskService: TaskService) { }
+
+  ngOnInit() {
+    this.taskService.getTasks();
+    this.resetForm()
+  }
+
+  // submit del formulario
+  enviarDatos(taskForm: NgForm){
+    this.taskService.insertTask(taskForm.value);
+    this.resetForm(taskForm);
+  }
+
+
+  resetForm(taskForm?: NgForm){
+    if(taskForm != null){
+      taskForm.reset();
+      this.taskService.selectedTask = new Task()
+    }
+  }
+}
